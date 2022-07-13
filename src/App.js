2a0a4c304ba63c0a9b8f98b78652df7df89619ee
Afra Anwar <afraanwar@Afras-Air.local>
2022-07-13 10:12:39 +0200

    Added my project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import react from "react";
+import "./App.css";
+import Login from "./Login";
+// 3a65fb5ed3fa40ffb8e11cc49f0d4c30
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/** Login with spotify button */}
+      <Login />
     </div>
   );
 }
